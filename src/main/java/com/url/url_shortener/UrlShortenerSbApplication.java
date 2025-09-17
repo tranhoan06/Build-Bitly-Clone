31fc4c23df898eaaa6afff2b2b0f9b65a708910c
viethoanf <90601825+tranhoan06@users.noreply.github.com>
2025-09-17 23:27:58 +0700

    update be

--- src/main/java/com/url/url_shortener/UrlShortenerSbApplication.java
+++ src/main/java/com/url/url_shortener/UrlShortenerSbApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UrlShortenerSbApplication {
-	// 10:11:39
+	// 10:11:40
 	// https://github.com/EmbarkXOfficial/url-shortener-project
 	public static void main(String[] args) {
 		SpringApplication.run(UrlShortenerSbApplication.class, args);
